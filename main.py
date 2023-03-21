b182abf1150fc80c43895d378a775af1b419601c
Muhammad Saad Ur Rehman <115045354+Partial-Programmer-Saad@users.noreply.github.com>
2023-03-21 17:12:27 +0000

    Displayed jobs dynamically by using dictionaries in python.

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 # Creating a module. A module name must be in lowercase.
 from flask import Flask, render_template
-
+# jobs Variable to store data about jobs instead of using Database(as of now)
+JOBS = [
+   {
+     "id" : "1",
+     "title" : "Data Scientist",
+     "location" : "Lahore, Pakistan",
+     "salary" : "120,000"
+   },
+   {
+    "id" : "2",
+     "title" : "Database Manager",
+     "location" : "Multan, Pakistan",
+     "salary" : "100,000"
+   },
+   {
+    "id" : "3",
+     "title" : "Data Entry",
+     "location" : "Karachi, Pakistan",
+     "salary" : "$7,000"
+   },
+  {
+    "id" : "3",
+     "title" : "Data Entry",
+     "location" : "Karachi, Pakistan",
+     
+   }
+      ]
 # Creating a variable of class Flask(OOP).
 app=Flask(__name__)
 
 # Creating a python decorator to route the homepage.
 @app.route("/")
 def main_jovian():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 if __name__=='__main__':
   # Access .run command by writing it as it was unaccessible.
