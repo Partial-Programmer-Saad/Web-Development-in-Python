cbbccaeb52a0b3ba61fbf55b4a9ab3a5a94232f0
Muhammad Saad Ur Rehman <115045354+Partial-Programmer-Saad@users.noreply.github.com>
2023-03-24 16:14:24 +0000

    Added Header and Footer.

--- main.py
+++ main.py
@@ -1,48 +1,43 @@
 # Creating a module. A module name must be in lowercase.
 from flask import Flask, render_template, jsonify
 # jobs Variable to store data about jobs instead of using Database(as of now)
-JOBS = [
-   {
-     "id" : "1",
-     "title" : "Data Scientist",
-     "location" : "Lahore, Pakistan",
-     "salary" : "120,000"
-   },
-   {
-    "id" : "2",
-     "title" : "Database Manager",
-     "location" : "Multan, Pakistan",
-     "salary" : "100,000"
-   },
-   {
-    "id" : "3",
-     "title" : "Data Entry",
-     "location" : "Karachi, Pakistan",
-     "salary" : "$7,000"
-   },
-  {
-    "id" : "3",
-     "title" : "Data Entry",
-     "location" : "Karachi, Pakistan",
-     
-   }
-      ]
+JOBS = [{
+  "id": "1",
+  "title": "Data Scientist",
+  "location": "Lahore, Pakistan",
+  "salary": "120,000"
+}, {
+  "id": "2",
+  "title": "Database Manager",
+  "location": "Multan, Pakistan",
+  "salary": "100,000"
+}, {
+  "id": "3",
+  "title": "Data Entry",
+  "location": "Karachi, Pakistan",
+  "salary": "$7,000"
+}, {
+  "id": "3",
+  "title": "Data Entry",
+  "location": "Karachi, Pakistan",
+}]
 # Creating a variable of class Flask(OOP).
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 # Creating a python decorator to route the homepage.
 @app.route("/")
 def main_jovian():
   return render_template('home.html', jobs=JOBS)
 
+
 @app.route("/jobs")
 def jobs_Jsonify():
   return jsonify(JOBS)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
   # Access .run command by writing it as it was unaccessible.
   app.run(host="0.0.0.0", debug=True)
   # Host="0.0.0.0" means that the program will run on local host(the pc being used)
   # debug=True means that it will keep on debugging itself
-  
-  
